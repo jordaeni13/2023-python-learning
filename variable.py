557246d60233b7bfcee90778574f95608c6b5750
jordaeni13 <70104240+jordaeni13@users.noreply.github.com>
2023-07-26 22:00:23 +0900

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,8 @@
+print((10000+10000)*2-10000)
+
+wallet = 10000 #wallet 변수가 10000이라는 값을 가르키도록 한다
+wallet = wallet + 10000 #기존 값에 10000을 더한 값을 가르키도록 한다
+wallet = wallet * 2
+wallet = wallet - 10000
+
+print(wallet)
